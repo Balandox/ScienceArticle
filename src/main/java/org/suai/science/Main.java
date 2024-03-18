d91fa2a3db63e8574b7131a0a218de871150ce12
Balandox <balandox221411@mail.ru>
2024-03-18 22:50:30 +0300

    smth

--- src/main/java/org/suai/science/Main.java
+++ src/main/java/org/suai/science/Main.java
@@ -15,16 +15,37 @@ public class Main {
         Boolean isGraphFullyConnected = false;
         DfsCalculation dfsCalculation = new DfsCalculation();
         do {
-            graph = GraphGeneration.generateGraph(5);
+            graph = GraphGeneration.generateGraph(6);
             dfsCalculation.setGraph(graph);
             isGraphFullyConnected = dfsCalculation.isFullyConnectedGraph();
         }
         while (!isGraphFullyConnected);
+        //System.out.println(graph);
+
+/*        int[][] matrix = {
+                {0,0,0,1,1,0},
+                {0,0,0,1,1,1},
+                {0,0,0,0,1,1},
+                {1,1,0,0,0,0},
+                {1,1,1,0,0,0},
+                {0,1,1,0,0,0}
+        };
+                int[][] matrix = {
+                {0,1,1,0,0,1},
+                {1,0,0,1,1,0},
+                {1,0,0,1,0,1},
+                {0,1,1,0,0,0},
+                {0,1,0,0,0,1},
+                {1,0,1,0,1,0}
+        };
+
+        AdjacencyMatrixGraph graph4 = new AdjacencyMatrixGraph(matrix, matrix.length, 0);*/
+        //AdjacencyMatrixGraph graph6 = new AdjacencyMatrixGraph(graph);
         System.out.println(graph);
-
-        GraphCycleCounter cycleCounter = new GraphCycleCounter(graph);
-        System.out.println("Amount of 4-Cycles: " + cycleCounter.countAmountCyclesWithLength(4));
-
+        GraphCycleCounter cycleCounter4 = new GraphCycleCounter(graph);
+        //GraphCycleCounter cycleCounter6 = new GraphCycleCounter(graph6);
+        System.out.println("Amount of 4-Cycles: " + cycleCounter4.countAmountCyclesWithLength(4));
+        //System.out.println("Amount of 6-Cycles: " + cycleCounter6.countAmountCyclesWithLength(6));
     }
 
 
--- src/main/java/org/suai/science/algorithms/GraphCycleCounter.java
+++ src/main/java/org/suai/science/algorithms/GraphCycleCounter.java
@@ -23,32 +23,82 @@ public class GraphCycleCounter {
         switch (length){
             case 4:
                 return countFourLengthCycles();
+            case 6:
+                return countSixLengthCycles();
             default:
                 return -1L;
         }
     }
 
-    private Long countFourLengthCycles(){
+    private Long countSixLengthCycles(){
         int[][] matrix = this.graph.getMatrix();
         int amountOfVertex = this.graph.getAmountOfVertex();
-        long w = CycleCounterUtil.calculatePossibleRowsCombination(2, amountOfVertex);
-        List<List<Integer>> combinations = CycleCounterUtil.generateCombinations(2, amountOfVertex);
+        List<List<Integer>> combinations = CycleCounterUtil.generateCombinations(3, amountOfVertex);
         //System.out.println(combinations);
         List<Long> Pi = new ArrayList<>();
 
         for(List<Integer> combination : combinations){
             int[] firstRow = matrix[combination.get(0)];
             int[] secondRow = matrix[combination.get(1)];
-            for(int i = 0; i < secondRow.length; i++)
-                if(secondRow[i] == 1)
-                    secondRow[i] = 2;
-            int[] sumRow = this.getSumRow(firstRow, secondRow);
+            int[] thirdRow = matrix[combination.get(2)];
+            this.replaceRowsElementsBeforeSum(6, secondRow, thirdRow);
+
+            int[] sumRow = this.getSumRow(firstRow, secondRow, thirdRow);
             int num3 = getAmountOfSpecifiedElemInRow(3, sumRow);
-            long pi = CycleCounterUtil.nCr(num3, 2);
+            int num5 = getAmountOfSpecifiedElemInRow(5, sumRow);
+            int num6 = getAmountOfSpecifiedElemInRow(6, sumRow);
+            int num7 = getAmountOfSpecifiedElemInRow(7, sumRow);
+            long pi = (((long) num3*num6*num5) + ((long) num7*num6*num5) + ((long) num3*num7*num5) + ((long) num3*num6*num7));
+            System.out.println("pi = " + pi + " on combination " + combination);
             Pi.add(pi);
         }
 
         return CycleCounterUtil.calculateTotalNumberOfCycles(Pi);
+    }
+
+
+    private Long countFourLengthCycles(){
+        int[][] matrix = this.graph.getMatrix();
+        int amountOfVertex = this.graph.getAmountOfVertex();
+        List<List<Integer>> combinations = CycleCounterUtil.generateCombinations(2, amountOfVertex);
+        List<Long> Pi = new ArrayList<>();
+
+        for(List<Integer> combination : combinations){
+            System.out.println("comb + " + combination);
+            //int[] firstRow = Arrays.copyOf(matrix[combination.get(0)], matrix[0].length);
+            //int[] secondRow = Arrays.copyOf(matrix[combination.get(1)], matrix[0].length);;
+            int[] firstRow = matrix[combination.get(0)];
+            int[] secondRow = matrix[combination.get(1)];
+            System.out.println("f " + Arrays.toString(firstRow));
+            System.out.println("s " + Arrays.toString(secondRow));
+            for(int i = 0; i < secondRow.length; i++)
+                if(secondRow[i] == 1)
+                    secondRow[i] = 2;
+            //this.replaceRowsElementsBeforeSum(4, secondRow);
+            System.out.println("sr " + Arrays.toString(secondRow));
+            int[] sumRow = this.getSumRow(firstRow, secondRow);
+            System.out.println("sumRow " + Arrays.toString(sumRow));
+            int num3 = getAmountOfSpecifiedElemInRow(3, sumRow);
+            System.out.println("num3 " + num3);
+            long pi = CycleCounterUtil.nCr(num3, 2);
+            System.out.println("pi " + pi);
+            Pi.add(pi);
+            System.out.println("\n");
+        }
+
+        return CycleCounterUtil.calculateTotalNumberOfCycles(Pi);
+    }
+
+    private void replaceRowsElementsBeforeSum(int cycleLength, int[]... rows){
+        switch (cycleLength){
+            case 4: // contain only one row for changing
+                this.changeRowElementsOnSpecified(2, rows[0]); // second row 1->2
+                break;
+            case 6:
+                this.changeRowElementsOnSpecified(2, rows[0]); // second row 1->2
+                this.changeRowElementsOnSpecified(4, rows[1]); // third row 1->4
+                break;
+        }
     }
 
     private int[] getSumRow(int[]... rows){
@@ -57,6 +107,12 @@ public class GraphCycleCounter {
             for(int i = 0; i < row.length; i++)
                 sumRow[i] += row[i];
         return sumRow;
+    }
+
+    private void changeRowElementsOnSpecified(int elemToReplaceOn, int[] row){
+        for(int i = 0; i < row.length; i++)
+            if(row[i] == 1)
+                row[i] = elemToReplaceOn;
     }
 
     private int getAmountOfSpecifiedElemInRow(int elem, int[] sumRow){
